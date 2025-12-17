55a9e536dabb3b30d2dd548c265bf4cdce7dd7be
Dhruv Patel <dhruvapatel14032004@gmail.com>
2025-12-16 21:30:21 -0400

    chenged cors

--- src/app.js
+++ src/app.js
@@ -17,13 +17,20 @@ const app = express();
 // -------------------
 // CORS FIX
 // -------------------
+import cors from "cors";
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your React app
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
+    origin: [
+      "http://localhost:5173",
+      "http://cloudfrontend1403.s3-website-us-east-1.amazonaws.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
+
 
 // Handle preflight requests
 app.options("*", cors());
